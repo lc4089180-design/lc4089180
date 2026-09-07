ae344e8ec78fced33e8e03533d556997dcfe16ae
lc4089180-design <lc4089180@gmail.com>
2026-09-07 16:41:32 +0530

    Delete session2/task3.py

--- session2/task3.py
+++
@@ -1,6 +0,0 @@
-#Write a Python script that takes the number of followers and posts as input for a YouTube channel, stores them in variables, and prints 'You have X followers and Y posts.' Make sure to convert the inputs to the correct data types.
-followers = int (input ("Enter number of followers: ")) 
-
-posts = int (input ("Enter number of posts: ")) 
-
-print ( f"You have {followers} followers and {posts} posts.") 
